9aab2a72358050823531df1d8e33d6fb082c38f2
Belen Ramos <belramgut@alum.us.es>
2018-11-28 19:36:50 +0100

    Creada estructura de pruebas

---
+++ test/server.test.js
@@ -0,0 +1,14 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe("Contacts API", () => {
+    it("Hola mundo de prueba", (done) => {
+        var x = 3;
+        var y = 5;
+        var resultado = x + y;
+        expect(resultado).to.equal(8);
+        done();
+    });
+});
+
+
